f2b5005ee9576ffea7990fe911a387cbb7134005
Yash Chhatre <ychhatre@gmail.com>
2024-06-22 13:58:31 -0700

    connected to s3 bucket

--- app.py
+++ app.py
@@ -1,28 +1,60 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, flash
+import boto3
 import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limit file size to 16MB
+app.secret_key = 'supersecretkey'  # Secret key for signing cookies
 
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+# AWS S3 configuration
+load_dotenv()
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.environ.get('AWS_DEFAULT_REGION')
+EXTERNAL_S3_BUCKET = os.environ.get('EXTERNAL_S3_BUCKET')
+print(EXTERNAL_S3_BUCKET)
+# Initialize boto3 client
+s3 = boto3.client(
+    's3', 
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION
+)
+
+
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'mkv'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
-def upload_form():
-    return render_template('upload.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
-def upload_video():
+def upload_file():
     if 'file' not in request.files:
-        return redirect(request.url)
+        flash('No file part')
+        return redirect(url_for('index'))
     file = request.files['file']
     if file.filename == '':
-        return redirect(request.url)
-    if file:
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(file_path)
-        return 'File successfully uploaded'
+        flash('No selected file')
+        return redirect(url_for('index'))
+    if file and allowed_file(file.filename):
+        filename = file.filename
+        try:
+            s3.upload_fileobj(
+                file,
+                EXTERNAL_S3_BUCKET,
+                filename,
+                ExtraArgs={"ContentType": file.content_type}
+            )
+            flash('File successfully uploaded to S3')
+        except Exception as e:
+            flash(f'Error uploading file: {e}')
+        return redirect(url_for('index'))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
