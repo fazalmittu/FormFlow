b639bf0ff90cfd889abb0ccffd812221707c6bc0
Saarth Gaonkar <saarthgao@gmail.com>
2024-06-22 17:04:10 -0700

    gpt prompting

--- critic/gptv.py
+++ critic/gptv.py
@@ -4,13 +4,15 @@ import time
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+
+from utils.gpt_prompt import CRITIQUE_SYSTEM_MESSAGE
 # import requests
 
 load_dotenv()
 
 client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
 
-reference_video = cv2.VideoCapture("data/output_video.mp4")
+reference_video = cv2.VideoCapture("data/reference_video.mp4")
 
 base64Frames = []
 while reference_video.isOpened():
@@ -20,10 +22,12 @@ while reference_video.isOpened():
     _, buffer = cv2.imencode(".jpg", frame)
     base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
 
-reference_video.release()
-print(len(base64Frames), "frames read.")
+smaller_ref = [frame[1] for frame in enumerate(base64Frames) if frame[0] % 3 == 0]
 
-uploaded_video = cv2.VideoCapture("data/output_video.mp4")
+reference_video.release()
+print(len(smaller_ref), "frames read.")
+
+uploaded_video = cv2.VideoCapture("data/freethrow.mp4")
 
 base64Frames2 = []
 while uploaded_video.isOpened():
@@ -33,31 +37,33 @@ while uploaded_video.isOpened():
     _, buffer = cv2.imencode(".jpg", frame)
     base64Frames2.append(base64.b64encode(buffer).decode("utf-8"))
 
+smaller_user = [frame[1] for frame in enumerate(base64Frames2) if frame[0] % 3 == 0]
+
 uploaded_video.release()
-print(len(base64Frames2), "frames read.")
+print(len(smaller_user), "frames read.")
 
 
 PROMPT_MESSAGES = [
     {
+        "role": "system",
+        "content": [
+            CRITIQUE_SYSTEM_MESSAGE
+        ]
+    },
+    {
         "role": "user",
         "content": [
-            """
-            These are frames from two videos. The first video is a reference video with good technique for basketball shooting form. 
-            The second video is my basketball shooting form. Your response must be in json format with three keys. The first key is 
-            reference_key_features, which is a list of the 5 most important frames from the first reference video. The second key is upload_key_frames, which also
-            has the 5 most important frames from the second reference video, and these frames should be used for comparison to the first 
-            reference video key frames. The last key is overall_critique, and this is your overall detailed critique of my basketball shooting form, given
-            the reference video and your own knowledge. Be fair in your critique.
-            """,
-            *map(lambda x: {"image": x, "resize": 768}, base64Frames),
-            *map(lambda x: {"image": x, "resize": 768}, base64Frames2),
+            CRITIQUE_SYSTEM_MESSAGE,
+            *map(lambda x: {"image": x, "resize": 350}, smaller_ref),
+            *map(lambda x: {"image": x, "resize": 350}, smaller_user),
         ],
-    },
+
+    } 
 ]
 params = {
     "model": "gpt-4o",
     "messages": PROMPT_MESSAGES,
-    "max_tokens": 200,
+    "max_tokens": 2000,
 }
 
 result = client.chat.completions.create(**params)
--- utils/gpt_prompt.py
+++ utils/gpt_prompt.py
@@ -1,3 +1,36 @@
 CRITIQUE_SYSTEM_MESSAGE = """
+You are a basketball coach named Coach Carter. You have just been given a video of your student taking a shot (or multiple).
+You have also been given a video of a pro basketball player with perfect shot form. Your job is to compare your student's shot to the model's shot.
+Draw key comparisons but also provide your own input based on what you know good shot form to look like. You will be actually extracting key frames from both videos to do further analysis. 
+To begin, look at the individual frames and only select the key ones from the pro/student. 
 
+Only count shots that have been released from the hands. Only count the shot as made if it goes through the net. 
+
+    Your response should be in the following format (json):
+    {
+        'pro_key_frames': List[Integer],
+        'student_key_frames': List[Integer],
+        'overall_critique': String,
+        'shot_attempts': Integer,
+        'shots_made': Integer,
+    }
+
+    PICK 5 FRAMES FOR PRO_KEY_FRAMES AND STUDENT_KEY_FRAMES EACH. (Ex: [2, 13, 17, 19, 30], [1, 14, 16, 23, 30])
+    'pro_key_frames' --> key shot frames to analyze (gather, aim, bend, shoot, follow through) FROM PRO VIDEO
+    'student_key_frames' --> key shot frames to analyze (gather, aim, bend, shoot, follow through) FROM STUDENT VIDEO
+    'overall_critique' --> Your overall analysis of the shot form taking into account all of the intermediate steps and the technique in the reference
+    video. Make sure the analysis is detailed and fair.
+
+    Give an intricate, detailed response. 
+
+"""
+
+CRITIQUE_USER_MESSAGE = """
+The 2 sets of frames are attached. Start by fusing the frames together to create 2 videos (one for pro and one for student). The first video is a reference video with good technique for basketball shooting form. 
+The second video is the student basketball shooting form. Your response must be in json format.
+
+Remember to start by extracting the frames and picking out the key frames that show the intermediate steps.
+Please do not just pick the first 10 frames, look more deeply. 
+
+Only count shots that have been released from the hands. Only count the shot as made if it goes through the net. 
 """
